c6ce5e86b01a49fc74f875cce2e8003f4a31d4c0
Januard Lumbangaol <januargaol48@gmail.com>
2024-06-24 14:46:31 +0700

    responsive greeting section

--- src/container/greetings-section/index.tsx
+++ src/container/greetings-section/index.tsx
@@ -28,49 +28,53 @@ export const GreetingSection = () => {
 
   return (
     <div ref={greetingRef} className="bg-[#F4F6F7] section-container">
-      <div className="h-full flex justify-center items-center space-x-16">
-        <div>
-          <p className="text-4xl leading-[58px]">Hey there,</p>
-          <p className="text-5xl font-bold leading-[58px]">
-            I’m Januard Lumbangaol,
-          </p>
-          <div className="flex items-center relative mt-[-10px]">
-            <p className="text-4xl font-bold leading-[58px] flex items-center absolute z-10">
-              A Frontend Developer
+      <div className="h-full w-full p-4 block items-center space-y-10 sm:flex sm:justify-center sm:space-x-16 sm:space-y-0 ">
+        <div className="greeting-content h-[50%] sm:h-full">
+          <div>
+            <p className="text-4xl leading-[58px]">Hey there,</p>
+            <p className="text-5xl font-bold leading-[58px]">
+              I’m Januard Lumbangaol,
             </p>
-            <div className="ml-80 mt-3">
-              <Player
-                src={codeAnimation}
-                autoplay
-                loop
-                style={{ height: "100px", width: "auto" }}
-              />
+            <div className="flex items-center relative mt-[-10px]">
+              <p className="text-2xl font-bold leading-[58px] flex items-center absolute z-10 sm:text-4xl">
+                Frontend Developer
+              </p>
+              <div className="ml-[172px] sm:ml-[280px] mt-3">
+                <Player
+                  src={codeAnimation}
+                  autoplay
+                  loop
+                  style={{ height: "100px", width: "auto" }}
+                />
+              </div>
+            </div>
+            <div className="flex items-center space-x-3">
+              <Link href={LINK_MASTER_DATA.CV} target="_blank">
+                <Button
+                  variant="Primary"
+                  icon={<PiArrowUpRightBold size={20} color="white" />}
+                >
+                  <p className="text-lg font-bold">Resume</p>
+                </Button>
+              </Link>
+              <Link href={LINK_MASTER_DATA.LinkedIn} target="_blank">
+                <IoLogoLinkedin size={48} color="#2A2C32" />
+              </Link>
+              <Link href={LINK_MASTER_DATA.Github} target="_blank">
+                <FaGithubSquare size={42} color="#2A2C32" />
+              </Link>
             </div>
           </div>
-          <div className="flex items-center space-x-3">
-            <Link href={LINK_MASTER_DATA.CV} target="_blank">
-              <Button
-                variant="Primary"
-                icon={<PiArrowUpRightBold size={20} color="white" />}
-              >
-                <p className="text-lg font-bold">Resume</p>
-              </Button>
-            </Link>
-            <Link href={LINK_MASTER_DATA.LinkedIn} target="_blank">
-              <IoLogoLinkedin size={48} color="#2A2C32" />
-            </Link>
-            <Link href={LINK_MASTER_DATA.Github} target="_blank">
-              <FaGithubSquare size={42} color="#2A2C32" />
-            </Link>
-          </div>
         </div>
-        <div>
-          <Image
-            src={profileImage}
-            width={507}
-            height={401}
-            alt="inage-profile"
-          />
+        <div className="greeting-content h-[50%] sm:h-full">
+          <div>
+            <Image
+              src={profileImage}
+              width={507}
+              height={401}
+              alt="inage-profile"
+            />
+          </div>
         </div>
       </div>
     </div>
