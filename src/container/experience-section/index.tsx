5621a895b48fffa7dc1312695c62e3daab10fdb2
Januard Lumbangaol <januargaol48@gmail.com>
2024-08-19 23:14:45 +0700

    add fadeup animation and typewriter

--- src/container/experience-section/index.tsx
+++ src/container/experience-section/index.tsx
@@ -3,12 +3,28 @@ import { Stepper } from "@/components";
 import { AppContext } from "@/pages/_app";
 import { AppContextType } from "@/pages/app";
 
+import { motion, Variants } from "framer-motion";
+
 // master data
 import { WORKING_EXPERIENCES } from "@/data-master/data-master";
 import clsx from "clsx";
 
 export const ExperienceSection = () => {
   const { experienceRef } = useContext(AppContext) as AppContextType;
+
+  const fadeUpVariants: Variants = {
+    offscreen: {
+      y: 300,
+    },
+    onscreen: {
+      y: 10,
+      transition: {
+        type: "spring",
+        bounce: 0.2,
+        duration: 1.5,
+      },
+    },
+  };
 
   return (
     <div
@@ -17,11 +33,20 @@ export const ExperienceSection = () => {
     >
       {WORKING_EXPERIENCES.toSorted((a, b) => b.id - a.id).map(
         (experience, index) => (
-          <Stepper
+          <motion.div
             key={`experience-${experience.id}`}
-            experience={experience}
-            isLast={index === WORKING_EXPERIENCES.length - 1}
-          />
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.8 }}
+          >
+            <motion.div variants={fadeUpVariants}>
+              <Stepper
+                key={`experience-${experience.id}`}
+                experience={experience}
+                isLast={index === WORKING_EXPERIENCES.length - 1}
+              />
+            </motion.div>
+          </motion.div>
         )
       )}
     </div>
